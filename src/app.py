3eadff835d1ffd21aaa35ca92129fcbb54c6d62e
Carlos <carlosacia2@gmail.com>
2025-06-23 18:35:06 +0200

    [MOD] Get the categories in main function

--- src/app.py
+++ src/app.py
@@ -1,1 +1,8 @@
-print("Hello, World!")
+from api_mercadona import get_categories
+
+if __name__ == "__main__":
+    try:
+        categories = get_categories()
+        print(categories)
+    except Exception as e:
+        print(f"An error occurred: {e}")
