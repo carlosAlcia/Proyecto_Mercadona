6c66cd8c92bdbe8b90d5fcdadbebdbcf7dd2c141
Carlos <carlosacia2@gmail.com>
2025-06-24 18:44:55 +0200

    [MOD] Renaming functions and sorting

--- src/api_mercadona.py
+++ src/api_mercadona.py
@@ -1,7 +1,112 @@
+# Created by Carlos Alvarez on 2025-06-23
+
 import requests
 
 URL_Categories = "https://tienda.mercadona.es/api/categories/"
 URL_Products = "https://tienda.mercadona.es/api/categories/SUBCATEGORY_ID/"
+
+
+def get_categories():
+    """Fetches categories from the Mercadona API.
+    
+    Returns:
+        dict: A dictionary containing the categories.
+        
+    """
+    response = requests.get(URL_Categories, headers=None, timeout = 30)
+    if response.status_code == 200:
+        return process_categories_json(response.json())
+    else:
+        raise Exception(f"Error fetching categories: {response.status_code} - {response.text}")
+    
+def get_subcategories(category_id):
+    """Gets subcategories for a given category from the Mercadona API.
+    
+    Args:
+        category_id (str): The ID of the category to fetch subcategories for.
+    Returns:
+        dict: A dictionary containing the subcategories for the specified category.
+        """
+    response = requests.get(URL_Categories, headers=None, timeout = 30)
+    if response.status_code == 200:
+        categories = process_categories_json(response.json())
+        if category_id in categories:
+            return categories[category_id]['subcategory']
+        else:
+            raise Exception(f"Category ID {category_id} not found in categories.")
+    else:
+        raise Exception(f"Error fetching categories: {response.status_code} - {response.text}")
+
+def get_types(subcategory_id):
+    """Extracts types of products from a subcategory id.
+    
+    Args:
+        subcategory_id (int): The id of a subcategory.
+        
+    Returns:
+        dict: A dictionary containing the types of products for the specified subcategory.
+        
+    """
+    products = get_products_by_subcategory(subcategory_id)
+    types = []
+    
+    for product in products.values():
+        category = product['type']
+        if category not in types:
+            types.append(category)
+    
+    return types
+     
+def get_products_by_subcategory(subcategory_id, type:str=None):
+    """Fetches products for a given subcategory from the Mercadona API.
+    
+    Args:
+        subcategory_id (int): The ID of the subcategory to fetch products for.
+    Returns:
+        dict: A dictionary containing the products for the specified subcategory.
+        
+    """
+    url = URL_Products.replace("SUBCATEGORY_ID", str(subcategory_id))
+    response = requests.get(url, headers=None, timeout=30)
+    if response.status_code == 200:
+        products = process_products_json(response.json(), return_type=True)
+        if type:
+            products = {k: v for k, v in products.items() if v['type'] == type}
+        return products
+    else:
+        raise Exception(f"Error fetching products: {response.status_code} - {response.text}")
+
+
+def process_products_json(json_data, return_type=True):
+    """Processes the JSON data from the Mercadona API to extract products.
+    
+    Args:
+        json_data (dict): The JSON data from the API response.
+        return_type (bool): If True, returns the product type inside the dict.
+                            If False, does not include the type.
+        
+    Returns:
+        dict: A dictionary containing the processed products. Format:
+            {
+                product_id: {
+                    'name': product_name,
+                    'price': product_price
+                    'type': product_type (if return_type is True)
+                }
+            }
+        
+    """
+    products = {}
+    for category in json_data.get('categories', []):
+        for product in category.get('products', []):
+            product_id = product['id']
+            products[product_id] = {
+                'name': product['display_name'],
+                'price': product['price_instructions']['bulk_price'],
+                'type': category['name']
+            }
+
+    return products
 
 def process_categories_json(json_data):
     """Processes the JSON data from the Mercadona API to extract categories.
@@ -37,125 +142,3 @@ def process_categories_json(json_data):
             }
 
     return categories
-
-
-def get_categories():
-    """Fetches categories from the Mercadona API.
-    
-    Returns:
-        dict: A dictionary containing the categories.
-        
-    """
-    response = requests.get(URL_Categories, headers=None, timeout = 30)
-    if response.status_code == 200:
-        return process_categories_json(response.json())
-    else:
-        raise Exception(f"Error fetching categories: {response.status_code} - {response.text}")
-    
-def get_subcategories(category_id):
-    """Gets subcategories for a given category from the Mercadona API.
-    
-    Args:
-        category_id (str): The ID of the category to fetch subcategories for.
-    Returns:
-        dict: A dictionary containing the subcategories for the specified category.
-        """
-    response = requests.get(URL_Categories, headers=None, timeout = 30)
-    if response.status_code == 200:
-        categories = process_categories_json(response.json())
-        if category_id in categories:
-            return categories[category_id]['subcategory']
-        else:
-            raise Exception(f"Category ID {category_id} not found in categories.")
-    else:
-        raise Exception(f"Error fetching categories: {response.status_code} - {response.text}")
-    
-
-def process_products_json(json_data):
-    """Processes the JSON data from the Mercadona API to extract products.
-    
-    Args:
-        json_data (dict): The JSON data from the API response.
-        
-    Returns:
-        dict: A dictionary containing the processed products. Format:
-            {
-                product_id: {
-                    'name': product_name,
-                    'price': product_price
-                }
-            }
-        
-    """
-    products = {}
-    for category in json_data.get('categories', []):
-        for product in category.get('products', []):
-            product_id = product['id']
-            products[product_id] = {
-                'name': product['display_name'],
-                'price': product['price_instructions']['bulk_price'],
-                'category': category['name']
-            }
-
-    return products
-
-def get_products(subcategory_id):
-    """Fetches products for a given subcategory from the Mercadona API.
-    
-    Args:
-        subcategory_id (int): The ID of the subcategory to fetch products for.
-    Returns:
-        dict: A dictionary containing the products for the specified subcategory.
-        
-    """
-    url = URL_Products.replace("SUBCATEGORY_ID", str(subcategory_id))
-    response = requests.get(url, headers=None, timeout=30)
-    if response.status_code == 200:
-        return process_products_json(response.json())
-    else:
-        raise Exception(f"Error fetching products: {response.status_code} - {response.text}")
-    
-
-def get_subsubcategory(subcategory_id):
-    """Extracts sub-subcategories from a dictionary of products.
-    
-    Args:
-        products (dict): A dictionary of products.
-        
-    Returns:
-        dict: A dictionary containing the sub-subcategories.
-        
-    """
-    products = get_products(subcategory_id)
-    sub_subcategories = []
-    
-    for product in products.values():
-        category = product['category']
-        if category not in sub_subcategories:
-            sub_subcategories.append(category)
-    
-    return sub_subcategories
-
-
-def get_more_cheap_product(products, category=None):
-    """Finds the cheapest product from a dictionary of products.
-    
-    Args:
-        products (dict): A dictionary of products.
-        
-    Returns:
-        tuple: A tuple containing the product ID and its price of the cheapest product.
-        
-    """
-    if not products:
-        return None, None
-    
-
-    # If a category is specified, filter products by that category
-    if category:
-        products_filtered = {k: v for k, v in products.items() if v['category'] == category}
-    
-    cheapest_product = min(products_filtered.items(), key=lambda item: float(item[1]['price']))
-    
-    return cheapest_product[0], products[cheapest_product[0]]['price']
-
