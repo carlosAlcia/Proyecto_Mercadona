86c6313c749fc9e22b83b83ca40ea69208e0dc84
Carlos <carlosacia2@gmail.com>
2025-06-24 18:45:37 +0200

    [ADD] New file to group products related functions

---
+++ src/utils_products.py
@@ -0,0 +1,23 @@
+# Created by Carlos Alvarez on 2025-06-24
+
+def get_more_cheap_product(products, type:str=None):
+    """Finds the cheapest product from a dictionary of products.
+    
+    Args:
+        products (dict): A dictionary of products.
+        
+    Returns:
+        tuple: A tuple containing the product ID and its price of the cheapest product.
+        
+    """
+    if not products:
+        return None, None
+    
+
+    # If a category is specified, filter products by that category
+    if type:
+        products_filtered = {k: v for k, v in products.items() if v['type'] == type}
+    
+    cheapest_product = min(products_filtered.items(), key=lambda item: float(item[1]['price']))
+    
+    return cheapest_product[0], products[cheapest_product[0]]['price']
