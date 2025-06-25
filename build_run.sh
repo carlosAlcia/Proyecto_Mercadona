6b32019ec92d90789e312244b6c9eff503951794
Carlos <carlosacia2@gmail.com>
2025-06-25 19:13:25 +0200

    [ADD] Bind secrets file when running container.

--- build_run.sh
+++ build_run.sh
@@ -1,2 +1,2 @@
 docker build -t mercadona:latest .
-docker run --rm -it mercadona:latest 
+docker run --rm -it --mount type=bind,source=./secrets.json,target=/secrets.json,readonly mercadona:latest 
