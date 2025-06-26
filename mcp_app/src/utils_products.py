c57f8c197d7e50df1869249659c6c128723a68c0
Carlos <carlosacia2@gmail.com>
2025-06-26 19:08:55 +0200

    [FIX] Solved bug in get_more_cheap_product

--- mcp_app/src/utils_products.py
+++ mcp_app/src/utils_products.py
@@ -15,8 +15,8 @@ def get_more_cheap_product(products, type:str=""):
 
     # If a category is specified, filter products by that category
     if type:
-        products_filtered = {k: v for k, v in products.items() if v['type'] == type}
+        products = {k: v for k, v in products.items() if v['type'] == type}
     
-    cheapest_product = min(products_filtered.items(), key=lambda item: float(item[1]['price']))
+    cheapest_product = min(products.items(), key=lambda item: float(item[1]['price']))
     
     return cheapest_product[0], products[cheapest_product[0]]['price']
