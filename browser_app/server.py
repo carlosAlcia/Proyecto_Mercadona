87b167c60ef39e189d230a1997e1a2bc0334101e
Carlos <carlosacia2@gmail.com>
2025-06-26 20:37:45 +0200

    [NEW] Connection with browser

--- browser_app/server.py
+++ browser_app/server.py
@@ -1,17 +1,26 @@
 from flask import Flask, request, jsonify
+from browser import Browser
 
 app = Flask(__name__)
 PORT = 8080
+BROWSER_LAUNCHED = False
+BROWSER = None
 
 @app.route('/create_cart', methods=['POST'])
 def create_cart():
     data = request.get_json()  
-    print("Received data:", data)
-    
-    response = {
-        "Response": "Received your data successfully"
-    }
-    return jsonify(response), 200
+    products_id = data.get('products', [])
+
+    global BROWSER_LAUNCHED
+    if not BROWSER_LAUNCHED:
+        BROWSER.open_mercadona(products_id)
+        BROWSER_LAUNCHED = True
+    else:
+        BROWSER.add_products_to_cart(products_id)
+
+
+    return jsonify('{"Response":"Send to the cart."}'), 200
 
 if __name__ == '__main__':
+    BROWSER = Browser()
     app.run(debug=True, port=PORT)
